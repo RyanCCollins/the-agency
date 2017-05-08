04f5224dfdf9b6938a4a9b5aa4365c2042bf7d84
Ryan Collins <rccproductions@mac.com>
2017-05-08 13:38:04 -0400

    Feat: update home tests

--- src/client/features/Home/__tests__/index.test.tsx
+++ src/client/features/Home/__tests__/index.test.tsx
@@ -5,9 +5,10 @@ import { mountToJson } from 'enzyme-to-json';
 import { Store } from 'redux';
 import { Provider } from 'react-redux';
 import mockStore from 'test/mockstore';
-import HomeContainer from '../';
 import theme from 'root/theming';
+import HomeContainer from '../';
 import { State } from '../state';
+import Presentation from '../presentation';
 
 const testState: State = {
   isLoading: false,
@@ -16,19 +17,21 @@ const testState: State = {
 };
 
 describe('HomeContainer ', () => {
-  let store: Store<State>;
-  beforeEach(() => {
-    store = mockStore({ home: testState });
-  });
+  const store: Store<State> = mockStore({ home: testState });
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <HomeContainer />
+      </ThemeProvider>
+    </Provider>,
+  );
 
   it('should create a snapshot', () => {
-    const wrapper = mount(
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <HomeContainer />
-        </Provider>
-      </ThemeProvider>,
-    );
     expect(mountToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render a Presentation component', () => {
+    expect(wrapper.find(Presentation).length).toBe(1);
   });
 });
