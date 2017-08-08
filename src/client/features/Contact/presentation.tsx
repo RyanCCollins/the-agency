90bb0ecb4238ce2ffbfa3853386bf878f6e1c4fd
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-08-07 22:00:12 -0400

    Update presentation.tsx

--- src/client/features/Contact/presentation.tsx
+++ src/client/features/Contact/presentation.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Section, Box } from 'ui';
+import { PageIntro } from 'components';
 import Form from 'muicss/lib/react/form';
 import Input from 'muicss/lib/react/input';
 import Textarea from 'muicss/lib/react/textarea';
 import Button from 'muicss/lib/react/button';
-import { PageIntro } from 'components';
 
 export default function ContactPresentation(): JSX.Element {
   return (
