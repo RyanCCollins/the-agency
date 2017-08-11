0bec506a5571e44f681fb725462db1fcf4c9bddc
Ryan Collins <rccproductions@mac.com>
2017-08-11 13:03:58 -0400

    Feat: add project seeds

--- cms/db/schema.rb
+++ cms/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809224732) do
+ActiveRecord::Schema.define(version: 20170810223903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.jsonb "content"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
