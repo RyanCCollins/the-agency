68141df6340281f7e2294e79f6439c2346a7dcf6
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-08-13 09:48:48 -0400

    Update json_type.rb

--- cms/app/graph/types/json_type.rb
+++ cms/app/graph/types/json_type.rb
@@ -1,6 +1,6 @@
 JsonType = GraphQL::ScalarType.define do
   name 'JSON'
-  description 'JSON Scalar Type, representing the Postgres jsonb data type'
+  description 'JSON Scalar Type, representing the Postgres jsonb data type.  Useful for representing hierarchical data.'
   coerce_input -> (x) { JSON.parse(x) }
   coerce_result -> (x) { JSON.dump(x) }
 end
